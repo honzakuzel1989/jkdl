4f1f1164c12505029b41e413b8f2a73163a0aded
Jan Kužel <jan.kuzel@alza.cz>
2020-08-05 11:14:21 +0200

    ConfigurationService refactoring.

--- jkdl/ConfigurationService.cs
+++ jkdl/ConfigurationService.cs
@@ -1,38 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
-using System.Collections.Generic;
-using System.IO;
-
-namespace jkdl
+﻿namespace jkdl
 {
     internal class ConfigurationService : IConfigurationService
     {
-        private static readonly string FILENAME = "filename";
-        private static readonly string LINK = "link";
-        private static readonly string OVERWRITE = "overwrite";
-
-        private static readonly Dictionary<string, string> _switchMappings = new Dictionary<string, string>()
-        {
-            { "-f", FILENAME },
-            { "-l", LINK },
-            { "-o", OVERWRITE },
-        };
-        
-        private readonly IConfigurationRoot _configurationRoot;
-
         public ConfigurationService(string[] args)
         {
-            _configurationRoot = new ConfigurationBuilder()
-                .AddCommandLine(args, _switchMappings)
-                .Build();
         }
-
-        public bool HasFilename => !string.IsNullOrWhiteSpace(_configurationRoot[FILENAME]);
-
-        public FileInfo GetFilename => new FileInfo(_configurationRoot[FILENAME]);
-
-        public bool HasLink => !string.IsNullOrWhiteSpace(_configurationRoot[LINK]);
-
-        public string GetLink => _configurationRoot[LINK];
 
         public bool OverwriteResult => true;
         public int MaxNumberOfDownload => 5;
